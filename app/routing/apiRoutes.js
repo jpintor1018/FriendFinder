4e0b9102906f15b63a1d5f3544f130daa3b115a8
John Michael Pintor <jpintor1018@yahoo.com>
2019-09-21 20:17:54 -0400

    problem with submit

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -11,6 +11,14 @@ module.exports = function(app){
       app.post("/api/friends", function(req,res){
           path.push(req.body)
           res.json(true)
+
+          for(var i=0;i<path.length;i++){
+            console.log(path[i].name)
+
+            for (var j=0;j<path.scores.length;j++){
+              
+            }
+          }
       }
         )
 }
