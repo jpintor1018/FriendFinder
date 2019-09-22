9c75cab80d3e73519d3d6ec7749945430f293902
John Michael Pintor <jpintor1018@yahoo.com>
2019-09-21 20:36:51 -0400

    added for loop

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,16 +9,26 @@ module.exports = function(app){
       //userData = req (survey.html)
 
       app.post("/api/friends", function(req,res){
+        var friend = {
+          name:"",
+          photo:"",
+        };
+        var newUser = req.body;
+        var newScores = newUser.scores;
+        var newName = newUser.name;
+        var newPhoto = newUser.photo;
+        
+        for(var i=0;i<path.length;i++){
+          console.log(path[i].name)
+          var scoreDiff = 0;
+
+          for (var j=0;j<10;j++){
+            scoreDiff += Math.abs(parseInt(newScore[j])-parseInt(path[i].scores[j]));
+          }
+        }
           path.push(req.body)
           res.json(true)
 
-          for(var i=0;i<path.length;i++){
-            console.log(path[i].name)
-
-            for (var j=0;j<path.scores.length;j++){
-              
-            }
-          }
       }
         )
 }
